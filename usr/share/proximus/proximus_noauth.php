839048372258706fad92de72b2840fda6069a79f
Alexander Ganster <alexander.ganster@gmail.com>
2010-11-26 13:15:44 +0100

    rework no-auth reloading script
    Signed-off-by: Alexander Ganster <alexander.ganster@gmail.com>

--- usr/share/proximus/proximus_noauth.php
+++ usr/share/proximus/proximus_noauth.php
@@ -1,36 +1,64 @@
 #!/usr/bin/php 
 <?php
-# read config file
-$config_file = '/etc/proximus/proximus.conf';
-$config = parse_ini_file($config_file) or die("Can't open file $config_file");
 
-# Open needed files
+$config_file = '/etc/proximus/proximus.conf';
 $filename_ip = "/etc/squid/noauth_ip.txt";
 $filename_dn = "/etc/squid/noauth_dn.txt";
-$file_ip = fopen($filename_ip, 'w') or die("Can't open file $filename_ip");
-$file_dn = fopen($filename_dn, 'w') or die("Can't open file $filename_dn");
+$dirty_config = false;
+
+$config = parse_ini_file($config_file) or error("Can't open file $config_file");
 
 # connect to mysql
-$dbh = mysql_connect($config['db_host'], $config['db_user'], $config['db_pass']) or die("Unable to connect to MySQL");
-$selected = mysql_select_db($config['db_name'],$dbh) or die("Could not select database");
+$dbh = mysql_connect($config['db_host'], $config['db_user'], $config['db_pass']) or error("Unable to connect to MySQL");
+$selected = mysql_select_db($config['db_name'],$dbh) or error("Could not select database");
 
-
-$result = mysql_query("SELECT sitename FROM noauth_rules WHERE type='IP'");
-while ($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
-   fwrite($file_ip, $row{'sitename'}."\n" );
-   #echo $row{'sitename'}."\n";
-}
-
-$result = mysql_query("SELECT sitename FROM noauth_rules WHERE type='DN'");
-while ($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
-   fwrite($file_dn, $row{'sitename'}."\n" );
-   #echo $row{'sitename'}."\n";
-}
-
-
-fclose($file_ip);
-fclose($file_dn);
-
+updateFile($filename_ip, "SELECT sitename FROM noauth_rules WHERE type='IP'");
+updateFile($filename_dn, "SELECT sitename FROM noauth_rules WHERE type='DN'");
 
 mysql_close($dbh);
 
+if ($dirty_config == true) {
+   exec("/etc/init.d/squid status", $out, $retval);
+   if ($retval == 0) {
+      #echo "Reloading Squid config now...";
+      exec("/etc/init.d/squid reload", $out, $retval);
+   }
+}
+
+
+
+###############################
+##### some proper functions
+
+function stop($text, $code=0) {
+   echo $text;
+   exit($code);
+}
+
+function error($text) {
+   stop($text, 1);
+}
+
+function updateFile($filename, $query) {
+   global $dirty_config;
+
+   $file_md5_pre = md5_file($filename);
+
+   $file = fopen($filename, 'w') or error("Can't open file $filename");
+
+   $result = mysql_query($query);
+   while ($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
+      fwrite($file, $row{'sitename'}."\n" );
+      #echo $row{'sitename'}."\n";
+   }
+
+   fclose($file);
+
+   if ( $file_md5_pre != md5_file($filename) ) {
+      $dirty_config = true;
+      #echo "MD5 changed for file $filename - reloading squid config later\n";
+   }
+}
+
+
+?>
