40be2ae438ad8b37629d8a10f5c8c0a8af4acb8a
Alexander Ganster <alexander.ganster@gmail.com>
2011-05-16 14:12:20 +0200

    allow to limit noauth rules to location and add an expiration date
    Signed-off-by: Alexander Ganster <alexander.ganster@gmail.com>

--- proximus-redirector/usr/share/proximus/proximus_noauth.php
+++ proximus-redirector/usr/share/proximus/proximus_noauth.php
@@ -1,9 +1,12 @@
 #!/usr/bin/php 
 <?php
 
+openlog("Proximus-noauth", LOG_PID | LOG_INFO, LOG_LOCAL0);
 $config_file = '/etc/proximus/proximus.conf';
+$squid_init_script = "/etc/init.d/squid";
 $filename_ip = "/etc/squid/noauth_ip.txt";
 $filename_dn = "/etc/squid/noauth_dn.txt";
+$hostname = trim(`hostname -f`);
 $dirty_config = false;
 
 $config = parse_ini_file($config_file) or error("Can't open file $config_file");
@@ -12,25 +15,38 @@ $config = parse_ini_file($config_file) or error("Can't open file $config_file");
 $dbh = mysql_connect($config['db_host'], $config['db_user'], $config['db_pass']) or error("Unable to connect to MySQL");
 $selected = mysql_select_db($config['db_name'],$dbh) or error("Could not select database");
 
-updateFile($filename_ip, "SELECT sitename FROM noauth_rules WHERE type='IP'");
-updateFile($filename_dn, "SELECT sitename FROM noauth_rules WHERE type='DN'");
+# get the location_id of our proxy; use 1 if none available
+$result = mysql_fetch_array( mysql_query("SELECT  location_id FROM proxy_settings WHERE fqdn_proxy_hostname = '$hostname'") );
+$location_id = $result['location_id'];
+if ( $location_id == null ) {
+      $location_id = 1;
+}
+#print $location_id;
+
+# update no-auth files
+updateFile($filename_ip, "SELECT sitename, description FROM noauth_rules WHERE type='IP' 
+      AND (location_id = $location_id OR location_id = 1 OR location_id IS NULL) 
+      AND (valid_until IS NULL OR valid_until > NOW()) ");
+
+updateFile($filename_dn, "SELECT sitename, description FROM noauth_rules WHERE type='DN' 
+      AND (location_id = $location_id OR location_id = 1 OR location_id IS NULL) 
+      AND (valid_until IS NULL OR valid_until > NOW()) ");
 
 mysql_close($dbh);
 
+# Reload squid if no-auth rules have changed
 if ($dirty_config == true) {
-   exec("/etc/init.d/squid status", $out, $retval);
+   exec("$squid_init_script status", $out, $retval);
    if ($retval == 0) {
-      #echo "Reloading Squid config now...";
-      openlog("Proximus-noauth", LOG_PID | LOG_PERROR, LOG_LOCAL0);
       syslog ( LOG_WARNING, "No-auth lists were updated. Reloading Squid config..." );
-      exec("/etc/init.d/squid reload", $out, $retval);
+      exec("$squid_init_script reload", $out, $retval);
    }
 }
 
 
 
 ###############################
-##### some proper functions
+##### some functions
 
 function stop($text, $code=0) {
    echo $text;
@@ -41,6 +57,7 @@ function error($text) {
    stop($text, 1);
 }
 
+# write the result of the mysql-query to a file
 function updateFile($filename, $query) {
    global $dirty_config;
 
@@ -50,15 +67,15 @@ function updateFile($filename, $query) {
 
    $result = mysql_query($query);
    while ($row = mysql_fetch_array($result,MYSQL_ASSOC)) {
-      fwrite($file, $row{'sitename'}."\n" );
-      #echo $row{'sitename'}."\n";
+      fwrite($file, $row{'sitename'} . "\n" );
+      #echo $row{'sitename'}."\n";   # debug
    }
 
    fclose($file);
 
    if ( $file_md5_pre != md5_file($filename) ) {
       $dirty_config = true;
-      #echo "MD5 changed for file $filename - reloading squid config later\n";
+      syslog ( LOG_WARNING, "MD5 changed for file $filename - reloading squid config later" );
    }
 }
 
